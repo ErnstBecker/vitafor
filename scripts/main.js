709dda12d4a9c28cc0ddc7cd46f3344ed143e9fb
ErnstBecker <ernst.becker.neto@gmail.com>
2025-02-20 12:07:06 -0300

    feat: add products carousel

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ import { initCarousel } from './modules/initCarousel.js';
 import './modules/initSignup.js';
 import './modules/initValidate.js';
 import initGreetings from './modules/initGreetings.js';
-import initTimer from './modules/initTimer.js'; // nova importação do timer
+import initTimer from './modules/initTimer.js';
 
 initActiveMenu();
 initCarousel();
 initGreetings();
-initTimer(); // inicia o timer
+initTimer();
